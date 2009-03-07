34f139bcd4458a678aec042d4692944ae48c2388
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-07 11:21:31 -0600

    [sfEasyAntispamPlugin] initial import
    git-svn-id: http://svn.symfony-project.com/plugins/sfEasyAntispamPlugin/trunk@16084 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfEasyAntispamPluginConfiguration.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * sfEasyAntispamPlugin configuration.
+ * 
+ * @package     sfEasyAntispamPlugin
+ * @subpackage  config
+ * @author      Your name here
+ * @version     SVN: $Id$
+ */
+class sfEasyAntispamPluginConfiguration extends sfPluginConfiguration
+{
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
